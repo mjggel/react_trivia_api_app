90f3106e7027e232d8917322d64f904f43a070e8
mjggel <luismiguelvieira015@gmail.com>
2023-09-30 15:26:20 -0300

    create template of loginPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Routes>
+      <Route path='/' element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
