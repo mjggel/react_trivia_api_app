98314ecbc3b49db077914baa0d12e6f67ac03cfc
mjggel <luismiguelvieira015@gmail.com>
2023-09-30 17:47:47 -0300

    update UserReducer and App. Finish logic in the LoginPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<LoginPage />} />
+      <Route path='/' element={<Navigate to='/login' />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
   );
 }
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -16,12 +16,63 @@ import {
   AiFillSetting,
   AiOutlineUserAdd,
   AiFillPlayCircle,
+  AiOutlineBorder,
+  AiOutlineCheckSquare,
 } from 'react-icons/ai';
 import React, { useState } from 'react';
 import trivia_logo from '../imgs/trivia_logo.png';
+import { useNavigate } from 'react-router-dom';
+import { connect, useDispatch } from 'react-redux';
+import { setUserName } from '../Redux/Reducers/UserReducer';
 
-export default function LoginPage() {
+function LoginPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const users = JSON.parse(localStorage.getItem('users')) || [];
   const [showPassword, setShowPassword] = useState(false);
+  const [invalid, setInvalid] = useState(false);
+  const [errMessage, setErrorMessage] = useState('');
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    rememberMe: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      setInvalid(true);
+      setErrorMessage('All fields are required');
+      return;
+    }
+
+    const authenticatedUser = users.find((user) => {
+      return (
+        user.username === formData.username &&
+        user.password === formData.password
+      );
+    });
+
+    if (!authenticatedUser) {
+      setInvalid(true);
+      setErrorMessage('Username or password is invalid');
+      return;
+    }
+    if (formData.rememberMe) {
+      authenticatedUser.rememberMe = formData.rememberMe;
+      localStorage.setItem('users', JSON.stringify(users));
+    }
+    dispatch(setUserName(authenticatedUser.name));
+    navigate('/game');
+  };
 
   return (
     <Modal show={true} size='md' centered>
@@ -41,7 +92,14 @@ export default function LoginPage() {
             <InputGroup.Text>
               <AiOutlineUser />
             </InputGroup.Text>
-            <Form.Control placeholder='username' />
+            <Form.Control
+              type='text'
+              name='username'
+              value={formData.username}
+              onChange={handleChange}
+              isInvalid={invalid}
+              placeholder='username'
+            />
           </InputGroup>
 
           <InputGroup id='login-password' className='mb-3'>
@@ -50,6 +108,10 @@ export default function LoginPage() {
             </InputGroup.Text>
             <Form.Control
               type={showPassword ? 'text' : 'password'}
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+              isInvalid={invalid}
               placeholder='password'
             />
             <OverlayTrigger
@@ -74,22 +136,55 @@ export default function LoginPage() {
               </Button>
             </OverlayTrigger>
           </InputGroup>
+
+          {formData.username && formData.password && (
+            <>
+              <Button
+                name='rememberMe'
+                value={formData.rememberMe}
+                onClick={() =>
+                  setFormData((prevState) => ({
+                    ...prevState,
+                    rememberMe: !formData.rememberMe,
+                  }))
+                }
+                variant='ouline-info'
+                style={{
+                  borderColor: 'transparent',
+                }}
+              >
+                {formData.rememberMe ? (
+                  <AiOutlineCheckSquare size={20} />
+                ) : (
+                  <AiOutlineBorder size={20} />
+                )}
+              </Button>
+              Remember Me
+            </>
+          )}
         </Form>
+        <span className='text-danger'>{errMessage}</span>
       </Modal.Body>
 
       <Modal.Footer className='justify-content-around'>
-        <Button variant='info'>
+        <Button variant='info' onClick={() => navigate('/settings')}>
           <AiFillSetting /> Settings
         </Button>
 
-        <Button variant='primary'>
-          <AiFillPlayCircle /> Play
+        <Button variant='primary' onClick={handleSubmit}>
+          <AiFillPlayCircle /> Start
         </Button>
 
-        <Button variant='secondary'>
+        <Button variant='secondary' onClick={() => navigate('/register')}>
           <AiOutlineUserAdd /> Register
         </Button>
       </Modal.Footer>
     </Modal>
   );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  setUserName: (name) => dispatch(setUserName(name)),
+});
+
+export default connect(null, mapDispatchToProps)(LoginPage);
