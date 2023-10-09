8036fe98d51b72d8009764d05d1e2bfc4924a3e6
mjggel <luismiguelvieira015@gmail.com>
2023-10-09 19:02:55 -0300

    update LoginPage, add /register oroute to App.js and create RegisterPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigate to='/login' />} />
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
     </Routes>
   );
 }
--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -6,6 +6,7 @@ import {
   OverlayTrigger,
   Tooltip,
   Button,
+  CloseButton,
 } from 'react-bootstrap';
 import {
   AiOutlineUser,
@@ -13,30 +14,44 @@ import {
   AiFillEye,
   AiFillEyeInvisible,
   AiOutlineUnlock,
-  AiFillSetting,
+  AiOutlineArrowLeft,
+  AiOutlineMail,
   AiOutlineUserAdd,
-  AiFillPlayCircle,
-  AiOutlineBorder,
-  AiOutlineCheckSquare,
+  AiOutlineForm,
 } from 'react-icons/ai';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import trivia_logo from '../imgs/trivia_logo.png';
+import user_template from '../imgs/user_template_img.png';
 import { useNavigate } from 'react-router-dom';
-import { connect, useDispatch } from 'react-redux';
-import { setUserName } from '../Redux/Reducers/UserReducer';
 
-function LoginPage() {
+export default function RegisterPage() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const users = JSON.parse(localStorage.getItem('users')) || {};
+  const fileInputRef = useRef();
+  const users = JSON.parse(localStorage.getItem('users')) || [];
   const [showPassword, setShowPassword] = useState(false);
   const [invalid, setInvalid] = useState(false);
   const [errMessage, setErrorMessage] = useState('');
   const [formData, setFormData] = useState({
+    userpicture: user_template,
+    name: '',
     username: '',
+    email: '',
     password: '',
     rememberMe: false,
   });
+
+  const handleUserPictureChange = ({ target }) => {
+    const file = target.files[0];
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+
+      setFormData((prevState) => ({
+        ...prevState,
+        userpicture: imageUrl,
+      }));
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -48,30 +63,19 @@ function LoginPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!formData.username || !formData.password) {
+    if (
+      !formData.username ||
+      !formData.password ||
+      !formData.name ||
+      !formData.email
+    ) {
       setInvalid(true);
       setErrorMessage('All fields are required');
       return;
     }
 
-    const authenticatedUser = users?.find((user) => {
-      return (
-        user.username === formData.username &&
-        user.password === formData.password
-      );
-    });
-
-    if (!authenticatedUser) {
-      setInvalid(true);
-      setErrorMessage('Username or password is invalid');
-      return;
-    }
-    if (formData.rememberMe) {
-      authenticatedUser.rememberMe = formData.rememberMe;
-      localStorage.setItem('users', JSON.stringify(authenticatedUser));
-    }
-    dispatch(setUserName(authenticatedUser.name));
-    navigate('/game');
+    localStorage.setItem('users', JSON.stringify([formData, ...users]));
+    navigate('/login');
   };
 
   return (
@@ -86,8 +90,61 @@ function LoginPage() {
       </Modal.Header>
 
       <Modal.Body className='text-center'>
-        <Modal.Title>Login</Modal.Title>
+        <Modal.Title>
+          Register
+          {formData.userpicture !== user_template && (
+            <CloseButton
+              style={{ position: 'absolute', right: '10px', top: '10px' }}
+              onClick={() =>
+                setFormData({ ...formData, userpicture: user_template })
+              }
+            />
+          )}
+        </Modal.Title>
         <Form>
+          <InputGroup
+            id='login-userpicture'
+            className='mb-3'
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Form.Control
+              type='file'
+              ref={fileInputRef}
+              key={formData.userpicture}
+              accept='image/*'
+              style={{ display: 'none' }}
+              onChange={handleUserPictureChange}
+            />
+            <Image
+              src={formData.userpicture}
+              style={{
+                height: '200px',
+                width: '200px',
+                cursor: 'pointer',
+              }}
+              onClick={() => fileInputRef.current.click()}
+              roundedCircle
+              alt='userpicture'
+            />
+          </InputGroup>
+
+          <InputGroup id='login-name' className='mb-3'>
+            <InputGroup.Text>
+              <AiOutlineForm />
+            </InputGroup.Text>
+            <Form.Control
+              type='text'
+              name='name'
+              value={formData.name}
+              onChange={handleChange}
+              isInvalid={invalid}
+              placeholder='your name'
+            />
+          </InputGroup>
+
           <InputGroup id='login-username' className='mb-3'>
             <InputGroup.Text>
               <AiOutlineUser />
@@ -99,6 +156,20 @@ function LoginPage() {
               onChange={handleChange}
               isInvalid={invalid}
               placeholder='username'
+            />
+          </InputGroup>
+
+          <InputGroup id='login-email' className='mb-3'>
+            <InputGroup.Text>
+              <AiOutlineMail />
+            </InputGroup.Text>
+            <Form.Control
+              type='text'
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+              isInvalid={invalid}
+              placeholder='email'
             />
           </InputGroup>
 
@@ -136,51 +207,19 @@ function LoginPage() {
               </Button>
             </OverlayTrigger>
           </InputGroup>
-
-          {formData.username && formData.password && (
-            <>
-              <Button
-                name='rememberMe'
-                value={formData.rememberMe}
-                onClick={() =>
-                  setFormData((prevState) => ({
-                    ...prevState,
-                    rememberMe: !formData.rememberMe,
-                  }))
-                }
-                variant='ouline-info'
-                style={{
-                  borderColor: 'transparent',
-                }}
-              >
-                {formData.rememberMe ? (
-                  <AiOutlineCheckSquare size={20} />
-                ) : (
-                  <AiOutlineBorder size={20} />
-                )}
-              </Button>
-              Remember Me
-            </>
-          )}
         </Form>
         <span className='text-danger'>{errMessage}</span>
       </Modal.Body>
 
-      <Modal.Footer className='justify-content-around'>
-        <Button variant='info' onClick={() => navigate('/settings')}>
-          <AiFillSetting /> Settings
+      <Modal.Footer className='justify-content-between'>
+        <Button variant='outline_info' onClick={() => navigate('/login')}>
+          <AiOutlineArrowLeft size={25} />
         </Button>
 
-        <Button variant='primary' onClick={handleSubmit}>
-          <AiFillPlayCircle /> Start
-        </Button>
-
-        <Button variant='secondary' onClick={() => navigate('/register')}>
+        <Button variant='secondary' onClick={handleSubmit}>
           <AiOutlineUserAdd /> Register
         </Button>
       </Modal.Footer>
     </Modal>
   );
 }
-
-export default connect()(LoginPage);
